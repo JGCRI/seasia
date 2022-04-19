87e9b637293ec72f0f39e9a490ae84a3e98d7981
Taryn Waite <taryn.waite17@gmail.com>
2022-04-19 17:42:50 -0400

    updating webpage Thailand

--- webpage/thailand_aggregate_groups.R
+++ webpage/thailand_aggregate_groups.R
@@ -8,6 +8,8 @@
 building_group <- c("comm cooling", "comm heating", "comm others", "resid cooling", "resid heating", "resid others")
 transport_group <- c("trn_aviation_intl", "trn_freight", "trn_freight_road", "trn_pass", "trn_pass_road", "trn_pass_road_LDV", "trn_pass_road_LDV_4W", "trn_shipping_intl", "H2 forecourt production")
 industry_group <- c("industry", "industrial energy use", "industrial feedstocks", "cement", "process heat cement", "N fertilizer", "H2 central production", "refining",   "industrial processes",  "gas processing", "unconventional oil production", "iron and steel", "alumina", "aluminum", "chemical", "chemical energy use", "chemical feedstocks", "construction", "construction energy use", "construction feedstocks", "mining energy use", "agricultural energy use")
+industry_group2 <- c("industrial energy use")
+
 elec_combustion_group <- c("elec_biomass (conv CCS)", "elec_biomass (conv)", "elec_biomass (IGCC CCS)","elec_biomass (IGCC)", "elec_coal (conv pul CCS)", "elec_coal (conv pul)","elec_coal (IGCC CCS)","elec_coal (IGCC)", "elec_gas (CC CCS)","elec_gas (CC)","elec_gas (steam/CT)", "elec_refined liquids (CC CCS)","elec_refined liquids (CC)","elec_refined liquids (steam/CT)")
 elec_group <- c(elec_combustion_group, "electricity", "backup_electricity", "csp_backup", "electricity_net_ownuse")
 ag_group <- c("Corn", "FiberCrop", "FodderGrass", "MiscCrop", "OilCrop", "OtherGrain", "PalmFruit", "Rice", "RootTuber", "SugarCrop", "Wheat", "Beef", "Dairy", "Pork", "Poultry", "SheepGoat", "FodderHerb")
@@ -39,8 +41,9 @@ gas_group_local <- c("natural_gas_industry_energy", "natural_gas_industry_other"
 liquids_group_local <- c("petroleum_premium", "petroleum_regular", "kerosene", "diesel", "liquefied_petroleum_gas", "petroleum_JP", "oil_fuel")
 
 # electricity consumption sectors
-building_group_local <- c("Residential", "Business", "Government and Non-Profit(1)")
-industry_group_local <- c("Industrial", "Agriculture")
+building_group_local <- c("Residential", "Business", "Government and Non-Profit(1)", "trade", "residential")
+industry_group_local <- c("Industrial", "Agriculture", "industry", "agriculture")
 other_group_local <- c("Other", "Free of Charge")
+transport_group_local <- c("transport")
 
 
